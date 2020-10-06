588f6b6d8f8485ddacc1d82920810935d1117a0d
Oliver Louis <oliverlouis@me.com>
2020-10-06 18:25:34 +0200

    add NotFound page component

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Page not found!</h2>
+      <p>
+        Page not found - maybe check out all our
+        <router-link to="'/coaches">coaches</router-link>
+      </p>
+    </base-card>
+  </section>
+</template>
